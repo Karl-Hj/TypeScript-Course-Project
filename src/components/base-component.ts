2b0e134e34c3d32ba92ffa65467c676fbac98771
b19karhj <sc2rena@gmail.com>
2024-10-21 11:54:41 +0200

    Splitting file into smaller files
    Using namespace to split files and using a bundle.js file for all the code once it compiles.

---
+++ src/components/base-component.ts
@@ -0,0 +1,37 @@
+namespace App {
+  export abstract class Component<
+    T extends HTMLElement,
+    U extends HTMLElement
+  > {
+    template: HTMLTemplateElement;
+    hostElement: T;
+    element: U;
+
+    constructor(
+      templateId: string,
+      hostElementId: string,
+      insertAtStart: boolean,
+      newElementId?: string
+    ) {
+      this.template = document.getElementById(
+        templateId
+      )! as HTMLTemplateElement;
+      this.hostElement = document.getElementById(hostElementId)! as T;
+
+      const clone = document.importNode(this.template.content, true);
+      this.element = clone.firstElementChild as U;
+      if (newElementId) {
+        this.element.id = newElementId;
+      }
+      this.attachToDOM(insertAtStart);
+    }
+    private attachToDOM(insertAtBeginning: boolean) {
+      this.hostElement.insertAdjacentElement(
+        insertAtBeginning ? "afterbegin" : "beforeend",
+        this.element
+      );
+    }
+    abstract configure(): void;
+    abstract renderContent(): void;
+  }
+}
---
+++ src/decorators/autobind.ts
@@ -0,0 +1,13 @@
+namespace App {
+  export function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+    const orginalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+      configurable: true,
+      get() {
+        const boundFn = orginalMethod.bind(this);
+        return boundFn;
+      },
+    };
+    return adjDescriptor;
+  }
+}
