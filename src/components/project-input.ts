5b40502d3ea53b6b00f089ff1703866a06bbf179
b19karhj <sc2rena@gmail.com>
2024-10-21 12:35:21 +0200

    Testing out other es6 import options
    Just playing around to see if I missed some

--- src/components/project-input.ts
+++ src/components/project-input.ts
@@ -1,6 +1,6 @@
-import { Component } from "./base-component.js";
-import { Validatable, validate } from "../util/validation.js";
-import { Autobind } from "../decorators/autobind.js";
+import Component from "./base-component.js";
+import * as Validation from "../util/validation.js";
+import { Autobind as Autobind } from "../decorators/autobind.js";
 import { projectState } from "../state/project-state.js";
 
 export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
@@ -32,25 +32,25 @@ export class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
     const enteredDescription = this.descriptionInputElement.value;
     const enteredPeople = this.peopleInputElement.value;
 
-    const titleValidatable: Validatable = {
+    const titleValidatable: Validation.Validatable = {
       value: enteredTitle,
       required: true,
     };
-    const descriptionValidatable: Validatable = {
+    const descriptionValidatable: Validation.Validatable = {
       value: enteredDescription,
       required: true,
       minLength: 5,
     };
-    const peopleValidatable: Validatable = {
+    const peopleValidatable: Validation.Validatable = {
       value: +enteredPeople,
       required: true,
       min: 1,
       max: 20,
     };
     if (
-      !validate(titleValidatable) ||
-      !validate(descriptionValidatable) ||
-      !validate(peopleValidatable)
+      !Validation.validate(titleValidatable) ||
+      !Validation.validate(descriptionValidatable) ||
+      !Validation.validate(peopleValidatable)
     ) {
       alert("Invalid input, please try again!");
       return;
